e4fc2ce425170c02aa772c22cf5077fe4da18517
asb1512 <andrew@olympicmedia.com>
2021-07-18 15:56:42 -0400

    commit tree 74c0d47fcd549f6266e172aaa312d0569c01d210

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -10,8 +10,15 @@ const Movies = () => {
       {movies.map((movie, index) => (
         <div key={index}>
           <h3>{movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => (
+              <li>{genre}</li>
+            ))}
+          </ul>
         </div>
       ))}  
+      
     </div>
   );
 };
