b3663a568b9bb676681de0b367e581d727695f88
asb1512 <andrew@olympicmedia.com>
2021-07-18 13:55:26 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { movies } from '../data';
 
+{console.log(movies[0].title)}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>{movie.title}</h3>
+        </div>
+      ))}  
     </div>
   );
 };
